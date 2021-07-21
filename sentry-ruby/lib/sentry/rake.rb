6e6e5c12564d8c0302be42ee7f71730fa2fde3d1
st0012 <stan001212@gmail.com>
2021-07-21 22:07:12 +0800

    Wrap rake task's execution with Hub#with_background_worker_disabled
    This makes sure all events, whether manually triggered or handled by the
    SDK, can be sent synchronously.

--- sentry-ruby/lib/sentry/rake.rb
+++ sentry-ruby/lib/sentry/rake.rb
@@ -3,6 +3,18 @@ require "rake/task"
 
 module Rake
   class Application
+
+    alias orig_top_level top_level
+    # we can't patch #run because the SDK might be initialized inside the Rakefile, which is loaded after #run is called.
+    # #top_level handles the actual task execution.
+    def top_level
+      return orig_top_level unless Sentry.initialized? && Sentry.get_current_hub
+
+      Sentry.get_current_hub.with_background_worker_disabled do
+        orig_top_level
+      end
+    end
+
     alias orig_display_error_messsage display_error_message
     def display_error_message(ex)
       Sentry.capture_exception(ex, hint: { background: false }) do |scope|
